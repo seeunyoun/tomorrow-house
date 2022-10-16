b0d69adf8efdc23b0fa14f0e993aa07fba8b37c0
Youn Se Eun <tpdms717@gmail.com>
2022-10-16 23:09:06 +0900

    🚧 Implement toggling bookmark button

--- js/order-cta.js
+++ js/order-cta.js
@@ -16,3 +16,30 @@ function closeOrderModal() {
 
 orderCtaBuyButton.addEventListener('click', openOrderModal)
 orderModalOverlay.addEventListener('click', closeOrderModal)
+
+orderCtaBookmarkButton.addEventListener('click', toggleOrderCtaBookmark)
+
+function toggleOrderCtaBookmark() {
+  const [icon, countSpan] = this.children
+  const count = Number(countSpan.innerText.replaceAll(',', ''))
+  const hasActiveClass = this.classList.contains('is-active')
+
+  if (hasActiveClass) {
+    icon.classList = 'ic-bookmark'
+  } else {
+    icon.classList = 'ic-bookmark-filled'
+  }
+
+  this.classList.toggle('is-active')
+  replaceNewCountText(hasActiveClass, count, countSpan)
+}
+
+const replaceNewCountText = (hasActiveClass, newCount, spanHtml) => {
+  if (hasActiveClass) {
+    newCount = newCount - 1
+  } else {
+    newCount = newCount + 1
+  }
+
+  spanHtml.innerText = newCount.toLocaleString()
+}
