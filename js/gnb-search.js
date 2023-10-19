1c70b4c17eaf1cfd86c873e2b3f20703742e14c0
Youn Se Eun <tpdms717@gmail.com>
2023-10-19 12:00:43 +0900

    Updates

---
+++ js/gnb-search.js
@@ -0,0 +1,52 @@
+const gnbSearch = document.querySelector('.gnb-search')
+const gnbSearchInput = gnbSearch.querySelector('input')
+const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
+
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
+const deleteButtonList = gnbSearchHistoryList.querySelectorAll('.delete-button')
+
+function openGnbSearchHistory() {
+  if (gnbSearchHistoryList.children.length === 0) return
+
+  if (!gnbSearchHistory.classList.contains('is-active')) {
+    window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside)
+  }
+  gnbSearchHistory.classList.add('is-active')
+}
+
+function closeGnbSearchHistory() {
+  gnbSearchHistory.classList.remove('is-active')
+  window.removeEventListener('click', closeGnbSearchHistoryOnClickingOutside)
+}
+
+function closeGnbSearchHistoryOnClickingOutside(e) {
+  if (!gnbSearch.contains(e.target)) {
+    closeGnbSearchHistory()
+  }
+}
+
+gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+
+function deleteAllSearchHistoryItems() {
+  gnbSearchHistoryList.innerHTML = ''
+  closeGnbSearchHistory()
+}
+
+deleteAllButton.addEventListener('click', deleteAllSearchHistoryItems)
+
+function delteSearchHistoryItem(e) {
+  e.stopPropagation()
+  const itemToDelete = this.parentNode
+  gnbSearchHistoryList.removeChild(itemToDelete)
+
+  if (gnbSearchHistoryList.children.length === 0) {
+    closeGnbSearchHistory()
+  }
+}
+
+deleteButtonList.forEach((button) => {
+  button.addEventListener('click', delteSearchHistoryItem)
+})
---
+++ js/order-cta.js
@@ -0,0 +1,46 @@
+const orderCta = document.querySelector('.order-cta')
+const [orderCtaBookmarkButton, orderCtaBuyButton] = orderCta.children
+
+const orderModal = document.querySelector('.order-form-modal')
+const orderModalOverlay = document.querySelector('.overlay')
+
+function openOrderModal() {
+  orderModal.classList.add('is-open')
+  orderModalOverlay.classList.add('is-active')
+}
+
+function closeOrderModal() {
+  orderModal.classList.remove('is-open')
+  orderModalOverlay.classList.remove('is-active')
+}
+
+orderCtaBuyButton.addEventListener('click', openOrderModal)
+orderModalOverlay.addEventListener('click', closeOrderModal)
+
+orderCtaBookmarkButton.addEventListener('click', toggleOrderCtaBookmark)
+
+function toggleOrderCtaBookmark() {
+  const [icon, countSpan] = this.children
+  const count = Number(countSpan.innerText.replaceAll(',', ''))
+  const hasActiveClass = this.classList.contains('is-active')
+
+  if (hasActiveClass) {
+    icon.classList = 'ic-bookmark'
+  } else {
+    icon.classList = 'ic-bookmark-filled'
+  }
+
+  this.classList.toggle('is-active')
+  replaceNewCountText(hasActiveClass, count, countSpan)
+}
+
+const replaceNewCountText = (hasActiveClass, newCount, spanHtml) => {
+  if (hasActiveClass) {
+    newCount = newCount - 1
+  } else {
+    newCount = newCount + 1
+  }
+
+  spanHtml.innerText = newCount.toLocaleString()
+  spanHtml.setAttribute('aria-label', `북마크 ${newCount.toLocaleString()}회`)
+}
